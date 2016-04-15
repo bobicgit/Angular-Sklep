e99f547b3455167d3ff067b3c769589ef34e1fff
beata111 <wolska_beata@wp.pl>
2016-04-15 11:51:27 +0200

    adding more than one piece of item to the cart, removing items from the cart, calculating the total cost

--- src/app/components/shoppingCart/shoppingCart.controller.js
+++ src/app/components/shoppingCart/shoppingCart.controller.js
@@ -12,22 +12,38 @@ angular
     function ShoppingCartController(ShoppingCart) {
 
         var vm = this;
+        vm.cartItems = [];
         vm.sum;
+        vm.updteCartItems = updateCartItems;
+        vm.removeItem = removeItem;
+        vm.addOneItem = addOneItem;
+        vm.removeOneItem = removeOneItem;
+        
     
-
-        vm.cartItems =  [{ id: 3,  category: "rtv",       name: "myszka",   price : 55 , count: 7, url: "http://img.cda.pl/obr/oryginalne/94a93000173e08a43ab806abcc4687c1.jpg"},
-            { id: 4,  category: "warzywa",   name: "marchew",  price : 3.5, count: 8, url: "http://naturalneoczyszczanie.pl/wp-content/uploads/2011/06/marchewka.jpg"},
-            { id: 5,  category: "słodycze",  name: "wafelki",  price : 2.8, count: 4, url: "http://swpanel.pl/userfiles/user_65525/images/427_Neap%202004%20solo.jpg"},
-            { id: 6,  category: "owoce",     name: "truskawki",price : 8,   count: 3, url: "https://bialczynski.files.wordpress.com/2013/06/aa-12529377small_strawberry__95536.jpg"}]
-
-        cartItems();
+        updateCartItems();
 
 
 
-        function cartItems() {
+        function updateCartItems() {
             vm.cartItems = ShoppingCart.readCart();
+            ShoppingCart.sum();
             vm.sum = ShoppingCart.readSum();
         }
+
+        function removeItem (item) {
+            ShoppingCart.remove(item);
+            updateCartItems();
+        }
+
+        function addOneItem (item) {
+            console.log('dodano jeden' + item);
+        }
+
+        function removeOneItem (item) {
+            console.log('usunięto jeden' + item);
+        }
+
+        
     }
 
 
--- src/app/components/shoppingCart/shoppingCartFactory.js
+++ src/app/components/shoppingCart/shoppingCartFactory.js
@@ -9,6 +9,7 @@ angular
     function ShoppingCart () {
 
         var cart = [];
+        
         var cartSum = 0;
 
         var factory = {
@@ -16,7 +17,8 @@ angular
             readCart: readCart,
             addToCart: addToCart,
             sum: sum,
-            readSum : readSum
+            readSum : readSum,
+            remove : remove
         }
 
         return factory;
@@ -31,13 +33,30 @@ angular
         }
 
         function addToCart (item) {
-            cart.push(item);
+
+            var added = false;
+
+            cart.forEach(function (cartItem) {
+                if (cartItem.id === item.id) {
+                    cartItem.amount += 1;
+                    console.log(cart);
+                    added = true;
+                    return;
+                } 
+            });
+
+            if (!added) {
+                item.amount = 1;
+                cart.push(item);
+                console.log(cart);
+            }
         }
+
 
         function sum () {
             cartSum = 0;
             cart.forEach(function(item) {
-                cartSum += item.price;
+                cartSum += item.price * item.amount;
             });
         }
 
@@ -45,9 +64,20 @@ angular
             return cartSum;
         }
 
-    }    
+
+        function remove (item) {
+            var l = cart.length;
+            for (var i = 0 ; i < l ; i ++ ) {
+                if (cart[i].id === item.id) {
+                    cart.splice(i, 1);
+                    return;
+                }
+            }
+        }
 
 
+
+     }   
 })();
 
 
